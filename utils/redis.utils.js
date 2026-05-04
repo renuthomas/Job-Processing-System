8fe95dec89faa7d1e766af300de0f1ca721aea24
renuthomas <96842019+renuthomas@users.noreply.github.com>
2026-05-05 01:17:36 +0530

    test: skip database connection in test environment

--- utils/redis.utils.js
+++ utils/redis.utils.js
@@ -6,6 +6,7 @@ const client=createClient({
     url:`redis://:${process.env.REDIS_PASSWORD}@127.0.0.1:6379`
 });
 client.on('error',err=>console.log(`Redis Client Error:${err}`));
-await client.connect();
-
+if (process.env.NODE_ENV !== 'test') {
+    await client.connect();
+}
 export {client as redisClient};
